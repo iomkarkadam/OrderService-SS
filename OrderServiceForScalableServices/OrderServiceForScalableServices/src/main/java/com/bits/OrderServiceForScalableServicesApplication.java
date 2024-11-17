11b096989866b03644a36166c5f819abe2423fd8
iomkarkadam <omkarsk.0101@gmail.com>
2024-11-17 17:01:05 +0530

    initial commit- added simple springboot app

---
+++ OrderServiceForScalableServices/OrderServiceForScalableServices/src/main/java/com/bits/OrderServiceForScalableServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceForScalableServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceForScalableServicesApplication.class, args);
+	}
+
+}
